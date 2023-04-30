ef021eb56eefd37904d6891f901a0579f3a8e483
Javier Rodrigo Galian <javier.galian.1997@gmail.com>
2023-04-30 02:31:07 -0300

    agregando boton next y prev en navPage con css

--- src/components/navPage.jsx
+++ src/components/navPage.jsx
@@ -1,24 +1,20 @@
-/*function NavPage(props) {
-    return (
-        <header className='d-flex justify-content-between'>
-            <p>pagina: {props.page}</p>
-            <button className='btn btn-primary btn-sm' onClick={() => { props.setPage(props.page + 1) }}> pagina {props.page + 1} </button>
-        </header>
-    )
-}*/
-
+import '../styles/navPage.css';
 
 function NavPage(props) {
-
-
     return (
-        <header className='d-flex justify-content-between'>
-            <p>pagina: {props.page}</p>
-            {
-                props.page < props.cantPage ? <button className='btn btn-primary btn-sm' onClick={() => { props.setPage(props.page + 1) }}> pagina {props.page+1}</button>
-                : <button className='btn btn-primary btn-sm' disabled > fin </button>
-            }
+        <header className='d-flex justify-content-center'>
             
+            {
+                props.page == 1 ? <button className='btn btn-primary button-prev' disabled> inicio </button>
+                    : <button className='btn btn-primary button-prev' onClick={() => { props.setPage(props.page - 1) }}> pagina {props.page - 1}</button>
+            }
+            <p className="text-white">pagina {props.page}</p>
+            {
+                props.page < props.cantPage ?
+                    <button className='btn btn-primary button-next' onClick={() => { props.setPage(props.page + 1) }}> pagina {props.page + 1}</button>
+                    : <button className='btn btn-primary button-next' disabled > fin </button>
+            }
+
         </header>
     )
 }
